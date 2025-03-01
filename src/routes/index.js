db7ff961ab8a14396467af47b31606890b110cdd
dereoprojects <140266740+dereoprojects@users.noreply.github.com>
2025-03-01 19:21:29 +0300

    added logger middleware for testing.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import authRoutes from "./authRoutes.js";
 import actionsRoutes from "./actionsRoutes.js";
 import { verifyWebhook } from "../middlewares/authMiddleware.js";
+import { actionLogger } from "../middlewares/loggerMiddleware.js";
 
 
 const router = express.Router();
 router.get("/", (_, res) => res.send("GHLSmartBuildApp is running..."));
 router.use("/auth", authRoutes);
-router.use("/actions", verifyWebhook, actionsRoutes);
+router.use("/actions", actionLogger, actionsRoutes);  // For testing. Remove for production.
+//router.use("/actions", verifyWebhook, actionsRoutes); // This is the production route.
 
 
 export default router;
