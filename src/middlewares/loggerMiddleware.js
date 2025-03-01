eec90dd98f90694ea697fa0f765a83be8d7003c2
dereoprojects <140266740+dereoprojects@users.noreply.github.com>
2025-03-01 19:34:45 +0300

    logger change

--- src/middlewares/loggerMiddleware.js
+++ src/middlewares/loggerMiddleware.js
@@ -2,12 +2,7 @@ import logger from "../config/logger.js";
 
 export const actionLogger = (req, res, next) => {
   logger.info("Action Request Details", {
-    url: req.url,
-    method: req.method,
-    headers: req.headers,
-    query: req.query,
-    body: req.body,
-    timestamp: new Date().toISOString()
+    ...req
   });
   next();
 };
