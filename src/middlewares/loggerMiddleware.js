1711dbdc36009703f05b4157798fb3b0c5b362de
dereoprojects <140266740+dereoprojects@users.noreply.github.com>
2025-03-01 20:22:25 +0300

    logging strings directly

--- src/middlewares/loggerMiddleware.js
+++ src/middlewares/loggerMiddleware.js
@@ -2,7 +2,14 @@ import logger from "../config/logger.js";
 
 export const actionLogger = (req, res, next) => {
   logger.info("Action Request Details", {
-    ...req
+    method: req.method,
+    url: req.url,
+    path: req.path,
+    params: req.params,
+    query: req.query,
+    body: JSON.stringify(req.body),
+    headers: JSON.stringify(req.headers),
+    ip: req.ip
   });
   next();
 };
